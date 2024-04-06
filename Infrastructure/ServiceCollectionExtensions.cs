8f51362c5cdda38f815804cf5fcebba5f1320b3f
Tomasz Sikora <sikora507@gmail.com>
2024-04-06 17:03:10 +0200

    remaining files

---
+++ Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Interfaces.Repositories;
+using Infrastructure.Configuration;
+using Infrastructure.Db;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.Configure<CosmosDbConfiguration>(configuration.GetSection("CosmosDb"));
+        services.AddDbContext<SagaPatternDbContext>();
+
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IOrderRepository, OrderRepository>();
+
+        return services;
+    }
+}
